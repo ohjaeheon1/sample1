5671c8be87ab40af30580747995de793f9126ff5
KOSMO) <qca1313@naver.com>
2024-11-11 15:34:58 +0900

    두번째 커밋

---
+++ com/example/HomeClass.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HomeClass {
+    public static void main(String[] args) {
+        System.out.println("이것은 추가된 내용");
+        System.out.println("설정된 내용");
+    }
+}
