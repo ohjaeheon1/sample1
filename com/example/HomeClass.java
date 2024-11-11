73372fe4353a3099d6ea507c34d1b8ecda8f3582
graykim <oklinekgh@gmail.com>
2024-11-11 15:45:20 +0900

    김근형이 수정한 메시지2

--- com/example/HomeClass.java
+++ com/example/HomeClass.java
@@ -4,6 +4,7 @@ public class HomeClass {
     public static void main(String[] args) {
         System.out.println("이것은 추가된 내용");
         System.out.println("설정된 내용");
+        System.out.println("이것은 김근형이 또 추가한 내용");
         System.out.println("이것은 김근형이 추가한 내용");
     }
 }
