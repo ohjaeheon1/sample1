020694214ebf42c8589baf79f4c70001e8f23626
ohjaeheon1 <148048031+ohjaeheon1@users.noreply.github.com>
2024-11-11 15:43:17 +0900

    오재헌이 수정

--- com/example/HomeClass.java
+++ com/example/HomeClass.java
@@ -5,5 +5,6 @@ public class HomeClass {
         System.out.println("이것은 추가된 내용");
         System.out.println("설정된 내용");
         System.out.println("이것은 김근형이 추가한 내용");
+        System.out.println("이것은 오재헌이 추가한 내용");
     }
 }
